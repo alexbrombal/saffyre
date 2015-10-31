c8ce7bb4f9553a5affb8211d877dfa4a576e9b01
Alex Brombal <alexb@commercehouse.com>
2015-10-31 10:59:39 -0500

    Remove vendor folder from git

--- src/Q.php
+++ src/Q.php
@@ -4,7 +4,6 @@ namespace Saffyre;
 
 /**
  * A utility wrapper class for accessing $_GET, $_POST, and $_COOKIE data.
- * This class will clean up all effects of magic quotes if they are enabled.
  *
  * // Get individual values:
  * $getName = Q::get('name');
@@ -22,20 +21,17 @@ namespace Saffyre;
  * @author Alex
  *
  */
-class Q extends BaseClass
+class Q
 {
 	public static $cookie_domain;
-
-	private static $sy, $gpc, $rt;
 
 	private static $_GET;
 	private static $_POST;
 	private static $_COOKIE;
 
-	public function __get($name)
-	{
-		return isset($this->$name) ? $this->$name : null;
-	}
+    public $get;
+    public $post;
+    public $cookie;
 
 	/**
 	 * Rebuilds a query string based on this object.
@@ -44,12 +40,10 @@ class Q extends BaseClass
 	 * @param array $except
 	 * @return string
 	 */
-	public function buildQuery($except = array())
+	public function buildQueryString($except = array())
 	{
-		$obj = clone $this;
+		$obj = clone $this->get;
 		if($except) {
-			debug_print_backtrace();
-			print_r($except);
 			if(is_string($except)) $except = explode(",", str_replace(" ", "", $except));
 			foreach($except as $key) unset($obj->$key);
 		}
@@ -57,45 +51,12 @@ class Q extends BaseClass
 	}
 
 
-
-	public static function fromRequest($method = null)
+	public function __construct()
 	{
-		$q = new Q();
-		$methods = func_get_args();
-		if(!$methods) $methods = array('cookie', 'post', 'get');
-
-		foreach($methods as $method)
-		{
-			switch(strtolower($method)) {
-				case 'get': $var = $_GET; break;
-				case 'post': $var = $_POST; break;
-				case 'cookie': $var = $_COOKIE; break;
-			}
-			if(empty($var)) continue;
-			foreach($var as $key => $value)
-				if(!isset($q->$key)) $q->$key = self::clean($value);
-		}
-
-		return $q;
-	}
-
-	public static function clean($value)
-	{
-		if(!isset(self::$sy)) self::$sy = (bool)ini_get('magic_quotes_sybase');
-		if(!isset(self::$gpc)) self::$gpc = (bool)get_magic_quotes_gpc();
-		if(!isset(self::$rt)) self::$rt = (bool)get_magic_quotes_runtime();
-
-		if(self::$sy) $stripslashes = 'stripslashes';
-		else $stripslashes = 'stripcslashes';
-
-		if(self::$sy || self::$gpc || self::$rt) {
-			if(is_array($value)) foreach($value as &$v) $v = self::clean($v);
-			else {
-				$value = $stripslashes($value);
-				if(function_exists('mb_detect_encoding') && mb_detect_encoding($value . 'a', 'UTF-8,ISO-8859-1') != 'UTF-8') $value = utf8_encode($value);
-			}
-		}
-		return $value;
+        $this->get = new BaseClass($_GET);
+        $this->post = new BaseClass($_POST);
+        $this->cookie = new BaseClass($_COOKIE);
+        $this->headers = new BaseClass(getallheaders());
 	}
 
 	public static function get($name = null)
--- src/Saffyre.php
+++ src/Saffyre.php
@@ -13,45 +13,6 @@ class Saffyre {
 
 	private static $path;
 
-	public static function __init()
-	{
-		//set_error_handler(array('\Saffyre\Saffyre', '__error_handler'));
-		//register_shutdown_function(array('\Saffyre\Saffyre', '__shutdown_handler'));
-	}
-
-	static function __error_handler($errno, $errstr, $errfile, $errline, $errcontext)
-	{
-		echo "Error ($errno):<br/>\n" .
-			"<b>$errstr</b><br/>\n" .
-			"File: <b>$errfile</b> (line $errline)<br/><br/>\n\n";
-	}
-
-	static function __shutdown_handler()
-	{
-		print_r(error_get_last());
-	}
-
-
-	/**
-	 * Adds a directory to php's include_path.
-	 *
-	 * @param string $path The absolute path of a directory to add to the include_path
-	 * @param boolean $recursive Indicates whether all subfolders of $path should also be added to the include_path
-	 */
-	public static function includePath($path, $recursive = true)
-	{
-		$path = rtrim($path, '/');
-		if(!is_dir($path)) return;
-		set_include_path(get_include_path() . ":$path");
-		if(!$recursive) return;
-		$r = opendir($path);
-		while($dir = readdir($r)) {
-			if($dir == '.' || $dir == '..' || substr($dir, 0, 1) == '_' || substr($dir, 0, 1) == '.') continue;
-			if(is_dir("$path/$dir"))
-				self::includePath("$path/$dir", true);
-		}
-	}
-
 
 	/**
 	 * Cleans $path (an array of path components or a string path) by removing empty values and url-decoding the values.
@@ -59,7 +20,7 @@ class Saffyre {
 	 * @param string|array $path The array of path components or string path to clean.
 	 * @param boolean $string True to return a string path, false to return an array of components
 	 * @return string|array The cleaned path, as a string or array
-	 * @uses Util
+	 * @uses Saffyre\Arrays
 	 */
 	public static function cleanPath($path, $string = false)
 	{
@@ -136,57 +97,13 @@ class Saffyre {
 
     public static function forceSSL()
     {
-    	$url = URL_BASE . ltrim(Saffyre::request(), '/');
-    	$ssl = preg_replace('/^http:/', 'https:', $url);
-    	if($ssl != $url) Saffyre::redirect($ssl);
+        if (!Saffyre::isSSL())
+        {
+            $url = URL_BASE . ltrim(Saffyre::request(), '/');
+            $ssl = preg_replace('/^http:/', 'https:', $url);
+            Saffyre::redirect($ssl);
+        }
     }
-
-    public static function requestBody()
-    {
-        return file_get_contents('php://input');
-    }
-
-    /**
-     * A default exception handler for the Saffyre framework.
-     *
-     * @param Exception $e
-     */
-	public static function exceptionHandler(Exception $e) {
-		echo "<br/><br/><b>{$e->getMessage()}</b><br/>".nl2br($e->getTraceAsString());
-	}
-
-	/**
-	 * Requires HTTP Authentication. Execution dies after authentication fails.
-	 *
-	 * @param string $message Message displayed to the user
-	 * @param array $creds An associative array of username => sha1(password) pairs.
-	 * @param callback $failure A callback method that is called if authentication fails.
-	 */
-	public static function requireHTTPAuth($message, $creds, $failure = null)
-	{
-		//if(isset($_COOKIE['auth'])) return true;
-
-		if(isset($_SERVER['PHP_AUTH_USER']))
-		{
-			if(isset($creds[$_SERVER['PHP_AUTH_USER']]) && sha1($_SERVER['PHP_AUTH_PW']) == $creds[$_SERVER['PHP_AUTH_USER']])
-			{
-				Q::cookie('auth', 1, 10000000);
-				return true;
-			}
-			else
-			{
-				header("WWW-Authenticate: Basic realm=\"$message\"", null, 401);
-				if($failure) call_user_func($failure);
-				die();
-			}
-		}
-		else
-		{
-			header("WWW-Authenticate: Basic realm=\"$message\"", null, 401);
-			die('Forbidden');
-		}
-	}
-
 
 	public static function forceTrailingSlash($slash = true)
 	{
@@ -224,7 +141,6 @@ class Saffyre {
 			define('ENCODING', 'UTF-8');
 
 		header('Content-type: text/html; charset='.ENCODING);
-		header("X-Powered-By: Saffyre Framework 2.0", true);
 
 		//include_once dirname(__FILE__) . '/Controller.php';
 		$controller = new Controller(true, self::$path);
@@ -253,5 +169,3 @@ class Saffyre {
 		else ob_flush();
 	}
 }
-
-Saffyre::__init();
